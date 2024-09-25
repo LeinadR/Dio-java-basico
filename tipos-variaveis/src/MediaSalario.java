73545094482520f97a65c0d322d29c096c0ef4f8
Daniel rodrigues <danielrodriguess4321@gmail.com>
2024-09-25 19:52:15 -0300

    exercicio de pratica

---
+++ tipos-variaveis/src/MediaSalario.java
@@ -0,0 +1,14 @@
+public class MediaSalario {
+    public static void main(String[] args) {
+        double media, n1, n2, n3, n4;
+
+        n1 = 1500.00;
+        n2 = 2000.00;
+        n3 = 1000.00;
+        n4 = 1900.00;
+
+        media = (n1 + n2 + n3 + n4) / 4;
+        System.out.println("A media salarial da empresa é de R$ " + media);
+    }
+    
+}
